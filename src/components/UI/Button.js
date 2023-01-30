8dbe84b1ffa4fd264b38ca96f1815ebda6537041
mlabato <m_labato@hotmail.com>
2023-01-30 15:26:45 -0300

    illustrations added

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,6 +1,7 @@
 const Button = (props) => {
   return (
-    <button
+    
+    <div
       className={
         props.classes ||
         "bg-[#1a1f16] text-[#fdffe6] hover:text-[#f5ff00] sm:text-[10px] md:text-[15px] sm:mx-[10px] md:mx-[30px] lg:mx-[60px] rounded-t-[8px] rounded-l-[8px] sm:w-[90px] sm:h-[20px] md:w-[118px] md:h-[30px] "
@@ -8,7 +9,8 @@ const Button = (props) => {
       onClick={props.onClick}
     >
       {props.children}
-    </button>
+    </div>
+   
   );
 };
 
--- src/components/layout/IntroWrapper.js
+++ src/components/layout/IntroWrapper.js
@@ -4,21 +4,24 @@ const IntroWrapper = () => {
   
 
   return (
-    <section className="bg-[#1a1f16] py-[3rem] notebook:py-[10rem] flex flex-col items-center  ">
-      <div className=" text-[3rem] notebook:text-[4rem] desktop:text-[8rem] my-[.25rem] notebook:my-[2rem] text-[#fdffe6] text-center font-bold  mx-auto flex flex-col notebook:flex-row">
-        <p className="mr-2 hover:text-[#f5ff00] hover:animate-transDown">escuchar.</p>
-        <p className="mr-2 hover:text-[#f5ff00] hover:animate-transDown">pensar. </p>
-        <p className="mr-2 hover:text-[#f5ff00] hover:animate-transDown">crear.</p>
+    <section className="bg-[#1a1f16] py-[3rem] notebook:py-[5rem] flex flex-row justify-around items-center px-[2rem] notebook:px-[8rem] desktop:px-[17rem]  ">
+    <article>
+      <div className=" text-[1.3rem] notebook:text-[3.2rem] desktop:text-[6rem] text-[#fdffe6] mb-[2rem]  font-bold   flex flex-col ">
+        <p className=" hover:text-[#f5ff00] hover:animate-transDown">escuchamos.</p>
+        <p className=" hover:text-[#f5ff00] hover:animate-transDown">pensamos. </p>
+        <p className=" hover:text-[#f5ff00] hover:animate-transDown">creamos.</p>
       </div>
 
-      <article className="w-full flex items-center justify-center mt-[2rem]">
-        <Button classes="bg-[#f5ff00] text-[#1a1f16] rounded-t-[8px] rounded-l-[8px] text-[.9rem] notebook:text-[1.2rem] desktop:text-[2.5rem]  px-[.5rem] desktop:px-[1.5rem] py-[.2rem] hover:px-[.6rem] hover:py-[.3rem] ease-in-out duration-300 absolute hover:animate-swing">
+      
+        <Button classes="bg-[#f5ff00] text-[#1a1f16] rounded-t-[8px] rounded-l-[8px] text-[.7rem] notebook:text-[1.2rem] desktop:text-[2.5rem]  px-[.5rem] desktop:px-[1.2rem] py-[.2rem] mx-auto w-2/3 notebook:w-1/2  text-center  ">
           <a alt="" href="#contact">
             
             contactanos
           </a>
         </Button>
+      
       </article>
+      <img src="/images/illustration.svg" alt="" className=" w-[10rem] notebook:w-[20rem] desktop:w-[30rem]"/>
     </section>
   );
 };
